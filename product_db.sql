73ddc3f47b6f9b4c062592a786811dedb0b74ee0
Ullas789 <Ullas78999@gmail.com>
2025-02-24 21:47:58 +0530

    updated the queries using join

--- product_db.sql
+++ product_db.sql
@@ -41,7 +41,7 @@ product.p_id,product.p_name,category.cat_name,category.cat_id,category.parent_id
  INNER JOIN brand on brand.b_id=product.b_id;
 
 
- -- 1.list all products
+-- 1.list all products
 select * from product;
 
 
@@ -59,7 +59,6 @@ select cat_id,p_name from product where cat_id=30;
 
 select product.p_name ,category.cat_name from product left join  category on product.cat_id=category.cat_id where category.cat_name='android';
 
-
 -- 5.list all category and their products
 -- select category.cat_name,product.p_name from product right join category on product.cat_id=category.cat_id;
 select category.cat_name,product.p_name from category left join product on product.cat_id=category.cat_id;
@@ -72,19 +71,25 @@ select product.p_name,brand.b_name from product left join brand on product.b_id=
 --1.list all sub categories under a category  based on cat_id
 select  cat_name as sub_cat,parent_id,cat_id from category where parent_id='40'
 
-
 --2.list all sub catagories under a list of categories based on id
 select  cat_id,cat_name  as sub_cat from category where parent_id='10'
 
 ---3.list all categories and sub_categories name
-select p.cat_name as parent_cat,c.cat_name from category c inner join category p on c.parent_id=p.cat_id;
+-- SELECT p.cat_name AS parent_cat,c.cat_name AS sub_cat 
+-- FROM category c,category p 
+-- WHERE c.parent_id=p.cat_id
 
-SELECT p.cat_name AS parent_cat,c.cat_name AS sub_cat 
-FROM category c,category p 
-WHERE c.parent_id=p.cat_id
+select p.cat_name as parent_cat,c.cat_name 
+from category c inner join category p 
+on c.parent_id=p.cat_id;
+
+
+
 
 
 --4.list all sub cat under a category based on cat_name
 SELECT p.cat_name AS parent_cat,c.cat_name AS sub_cat 
-FROM category c,category p 
-WHERE c.parent_id=p.cat_id and p.cat_name='electronics'
+FROM category c inner join category p 
+on c.parent_id=p.cat_id and p.cat_name='electronics';
+
+
